afd9872622dcb5e12a72501df56ebc3ea8bd3277
kamauchi yu <kamauchi_yu@YG40-04974.local>
2015-02-19 21:50:18 +0900

    とりあえずレイアウトと初期画面

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Kotonoki
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w( cosmo.css cosmo.js )
   end
 end
